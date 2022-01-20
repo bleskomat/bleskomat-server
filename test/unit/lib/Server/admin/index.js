5d372d43fcf1c0eaae4023177791269fd3e7551a
Charles Hill <chill@degreesofzero.com>
2022-01-20 18:55:58 +0000

    Use hashed password when both plaintext and hash provided

--- test/unit/lib/Server/admin/index.js
+++ test/unit/lib/Server/admin/index.js
@@ -45,18 +45,40 @@ describe('admin', function() {
 
 		it('with plaintext password', function() {
 			let config = this.helpers.prepareConfig();
-			const secret = 'test';
+			const password = 'test';
 			config.admin.web = true;
 			config.admin.password = null;
-			config.admin.passwordPlaintext = secret;
+			config.admin.passwordPlaintext = password;
 			return this.helpers.createServer(config).then(result => {
 				server = result;
 				const hash = server.app.custom.config.admin.password;
 				expect(hash).to.be.a('string');
 				expect(hash).to.not.equal('');
 				expect(server.app.custom.config.admin.passwordPlaintext).to.equal(null);
-				return scrypt.compare(secret, hash).then(correct => {
+				return scrypt.compare(password, hash).then(correct => {
 					expect(correct).to.equal(true);
+				});
+			});
+		});
+
+		it('with both hashed and plaintext password', function() {
+			let config = this.helpers.prepareConfig();
+			const password = 'test';// matches the hashed password below
+			const newPassword = 'test2';
+			config.admin.web = true;
+			config.admin.password = '8d2ae4b900956fc55cecd824213fc7d433d5e8ec0c554ca9c97e896f1e736157;4096;83da6e19afea72acff9526f959408b284bcdff0e7795912a13dbab3229225813';
+			config.admin.passwordPlaintext = newPassword;
+			return this.helpers.createServer(config).then(result => {
+				server = result;
+				const hash = server.app.custom.config.admin.password;
+				expect(hash).to.be.a('string');
+				expect(hash).to.not.equal('');
+				expect(server.app.custom.config.admin.passwordPlaintext).to.equal(null);
+				return scrypt.compare(password, hash).then(correct => {
+					expect(correct).to.equal(true);
+					return scrypt.compare(newPassword, hash).then(correct2 => {
+						expect(correct2).to.equal(false);
+					});
 				});
 			});
 		});
